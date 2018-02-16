ce3086315336deb1ef1982f958ba8c6e00f11029
cryptojatt <harpinder@sanghera.me.uk>
2018-02-16 00:09:52 +0000

    Update install_shekel.sh
    Install version 2.0
    
    -- Added option to install shekel as a service
    -- Added option to install a check (with optional email notification, as long as sendmail is configured correctly)

--- install_shekel.sh
+++ install_shekel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright Cryptojatt(c) 2018 
 # https://github.com/cryptojatt
-# install_shekel.sh version 1.8
+# install_shekel.sh version 2.0
 # Donation address: JQJ1GanDU3c5RZwNjBXk68wFdxEJKLwWZU
 # 
 # Created for shekel.io
@@ -46,8 +46,7 @@ configure_ubuntu () {
 	read -r rpcuser
 	echo -n "enter the rpcpassword		:"
 	read -r rpcpassword
-	echo -n "enter the externalip (IP ONLY, do not enter :5500 after the IP)	:"
-	read -r externalip
+	externalip=`curl -s http://whatismyip.akamai.com`
 	echo -n "enter the masternodeprivatekey		:"
 	read -r masternodeprivkey
 	echo "These were your answers		:"
@@ -183,8 +182,7 @@ configure_centos () {
 	read -r rpcuser
 	echo -n "enter the rpcpassword		:"
 	read -r rpcpassword
-	echo -n "enter the externalip (IP ONLY, do not enter :5500 after the IP)	:"
-	read -r externalip
+	externalip=`curl -s http://whatismyip.akamai.com`
 	echo -n "enter the masternodeprivatekey		:"
 	read -r masternodeprivkey
 	echo "These were your answers		:"
@@ -308,7 +306,7 @@ then
 		# Patches the system, installs required packages and repositories
 		apt-get update &&
 		apt-get upgrade -y &&
-		apt-get install wget nano unrar unzip libboost-all-dev libevent-dev software-properties-common libzmq3 libminiupnpc-dev -qy
+		apt-get install wget curl nano unrar unzip libboost-all-dev libevent-dev software-properties-common libzmq3 libminiupnpc-dev -qy
 		if [ ! -e /etc/apt/sources.list.d/bitcoin-bitcoin-trusty.list ]
 			then 
 		    	add-apt-repository ppa:bitcoin/bitcoin -y
@@ -334,7 +332,7 @@ then
 		echo "Installing Shekel on 16.04 from scratch"
 		apt-get update &&
 		apt-get upgrade -y &&
-		apt-get install wget nano unrar unzip libboost-all-dev libevent-dev software-properties-common libzmq3-dev libminiupnpc-dev -qy
+		apt-get install wget curl nano unrar unzip libboost-all-dev libevent-dev software-properties-common libzmq3-dev libminiupnpc-dev -qy
 		if [ ! -e /etc/apt/sources.list.d/bitcoin-ubuntu-bitcoin-xenial.list ]
 			then 
 		    	add-apt-repository ppa:bitcoin/bitcoin -y &&
@@ -362,7 +360,7 @@ then
 		yum clean all &&
 		yum repolist all &&
 		yum -y -q update &&
-		yum -y -q install unzip miniupnpc-devel zeromq libdb4-cxx
+		yum -y -q install unzip wget curl miniupnpc-devel zeromq libdb4-cxx
 		wget https://github.com/cryptojatt/JewNew/files/1716935/shekel-CentOS7-1.3.0.zip
 		unzip shekel-Ubuntu16.04-1.3.0.zip &&
 		rm shekel-Ubuntu16.04-1.3.0.zip &&
@@ -387,6 +385,272 @@ upgrade_masternode () {
 	echo "Upgrades not supported yet" # This will be added in a later version of this script
 }
 
+install_service () {
+echo "Installing systemd script to start at boot and start shekeld...please wait"
+sleep 5
+cat <<EOF > /etc/systemd/system/shekel.service
+[Unit]
+Description=Shekel's distributed currency daemon
+After=network.target
+
+[Service]
+User=$USER
+
+
+Type=forking
+PIDFile=/home/$USER/.shekel/shekeld.pid
+ExecStart=/usr/local/bin/shekeld -daemon -pid=/home/$USER/.shekel/shekeld.pid -conf=/home/$USER/.shekel/shekel.conf -datadir=/home/$USER/.shekel
+#-disablewallet
+
+Restart=always
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=2s
+StartLimitInterval=120s
+StartLimitBurst=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "script installed"
+sleep 3
+systemctl enable shekel.service
+echo "Script enabled"
+sleep 3
+echo "Checking if shekeld is running..."
+SERVICE="shekeld"
+RESULT=`ps -a | sed -n /${SERVICE}/p`
+if [ "${RESULT:-null}" = null ]; then
+    echo "Shekel is not running"
+else
+    echo "Stopping shekeld"
+    shekel-cli stop
+    sleep 3
+    RESULT2=`ps -a | sed -n /${SERVICE}/p`
+    if [ "${RESULT:-null}" = null ]; then
+    	echo "Shekel is not running"
+    else
+        echo "Stopping shekeld"
+	killall -9 shekeld
+    fi
+    
+fi
+echo "Starting the shekel.service service"
+echo "Please wait 30 seconds"
+systemctl start shekel.service
+sleep 30
+echo "Service should have started which you can see below"
+echo ""
+systemctl status shekel.service
+sleep 3
+echo "The service should show green above"
+echo ""
+sleep 3
+echo "Status of shekel-cli getinfo..."
+sleep 1
+shekel-cli getinfo
+sleep 1
+echo "Status of masternode status"
+shekel-cli masternode status
+sleep 3
+echo "done"
+echo ""
+echo "Going back to main menu"
+sleep 3
+}
+
+install_check () {
+echo "Creating check.sh in $USER's home directory"
+echo -n "Please paste your wallet address holding your 25000 shekel collateral	:"
+read -r add
+echo "Enter your email address if you want notifications	:"
+echo "You will need sendmail installed and configured"
+read -r email
+cat <<EOF  > /home/$USER/check.sh
+#!/bin/bash
+
+ipaddr=`curl -s http://whatismyip.akamai.com`
+echo "Your External IP Address is..."
+echo $ipaddr
+
+abort()
+{
+    echo >&2 '
+========================================
+=== Shekeld not running...restarting ===
+========================================
+'
+rm /home/$USER/.shekel/mncache.dat -rf
+systemctl start shekeld
+sleep 30
+echo ""
+echo ""
+echo "==== getinfo OUTPUT ===="
+   shekel-cli getinfo
+echo "==== getinfo OUTPUT ===="
+echo ""
+echo ""
+  sleep 30
+echo "==== masternode status OUTPUT ===="
+shekel-cli masternode status
+echo "==== masternode status OUTPUT ===="
+echo ""
+echo ""
+echo "==== masternode list OUTPUT ===="
+shekel-cli masternode list $add
+echo "==== masternode list OUTPUT ===="
+echo ""
+echo ""
+echo "==== debug.log OUTPUT ===="
+cat /home/$USER/.shekel/debug.log | grep CActiveMasternode::EnableHotColdMasterNode
+echo "==== debug.log OUTPUT ===="
+echo ""
+echo ""
+echo "You should see the following message above	:	"
+echo "Enabled! You may shut down the cold daemon."
+echo ""
+echo "This means the masternode is Enabled"
+echo ""
+echo ""
+}
+
+trap 'abort' 0
+
+set -e
+
+running=`shekel-cli getinfo | grep version`
+if [[ $running == *"version"* ]]; then
+   echo "JEW RUNNING!"
+   echo "============"
+echo ""
+echo ""
+echo "==== getinfo OUTPUT ===="
+   shekel-cli getinfo
+echo "==== getinfo OUTPUT ===="
+echo ""
+echo ""
+  sleep 30
+echo "==== masternode status OUTPUT ===="
+shekel-cli masternode status
+echo "==== masternode status OUTPUT ===="
+echo ""
+echo ""
+echo "==== masternode list OUTPUT ===="
+shekel-cli masternode list $add
+echo "==== masternode list OUTPUT ===="
+echo ""
+echo ""
+echo "==== debug.log OUTPUT ===="
+cat ~/.shekel/debug.log | grep CActiveMasternode::EnableHotColdMasterNode
+echo "==== debug.log OUTPUT ===="
+echo ""
+echo ""
+echo "You should see the following message above	:	"
+echo "Enabled! You may shut down the cold daemon."
+echo ""
+echo "This means the masternode is Enabled"
+echo ""
+echo ""
+fi
+
+check=`shekel-cli masternode list $add | grep ENABLED`
+echo $check
+if [[ $check == *"POS_ERROR"* ]]; then
+   echo "POS ERROR!"
+   systemctl stop shekeld
+   sleep 30
+   rm /home/$USER/.shekel/mncache.dat -rf
+   systemctl start shekel
+   sleep 30
+echo ""
+echo ""
+echo "==== getinfo OUTPUT ===="
+   shekel-cli getinfo
+echo "==== getinfo OUTPUT ===="
+echo ""
+echo ""
+  sleep 30
+echo "==== masternode status OUTPUT ===="
+shekel-cli masternode status
+echo "==== masternode status OUTPUT ===="
+echo ""
+echo ""
+echo "==== masternode list OUTPUT ===="
+shekel-cli masternode list $add
+echo "==== masternode list OUTPUT ===="
+echo ""
+echo ""
+echo "==== debug.log OUTPUT ===="
+cat ~/.shekel/debug.log | grep CActiveMasternode::EnableHotColdMasterNode
+echo "==== debug.log OUTPUT ===="
+echo ""
+echo ""
+echo "You should see the following message above	:	"
+echo "Enabled! You may shut down the cold daemon."
+echo ""
+echo "This means the masternode is Enabled"
+echo ""
+echo ""
+
+echo $check
+
+fi
+if [[ $check == *"ENABLED"* ]]; then
+   echo "JEW ENABLED!"
+echo ""
+echo ""
+echo "==== getinfo OUTPUT ===="
+   shekel-cli getinfo
+echo "==== getinfo OUTPUT ===="
+echo ""
+echo ""
+  sleep 30
+echo "==== masternode status OUTPUT ===="
+shekel-cli masternode status
+echo "==== masternode status OUTPUT ===="
+echo ""
+echo ""
+echo "==== masternode list OUTPUT ===="
+shekel-cli masternode list $add
+echo "==== masternode list OUTPUT ===="
+echo ""
+echo ""
+echo "==== debug.log OUTPUT ===="
+cat ~/.shekel/debug.log | grep CActiveMasternode::EnableHotColdMasterNode
+echo "==== debug.log OUTPUT ===="
+echo ""
+echo ""
+echo "You should see the following message above	:	"
+echo "Enabled! You may shut down the cold daemon."
+echo ""
+echo "This means the masternode is Enabled"
+echo ""
+echo ""
+
+fi
+
+trap : 0
+
+EOF
+
+if [ -z "$email" ]
+then
+	chmod +x /home/$USER/chech.sh
+	crontab -l > mycron
+	echo "*/30 * * * * /home/$USER/check.sh 2>&1 | tee output.exe | mail -s "Shekel Masternode status" $email"
+	crontab mycron
+	rm mycron
+else
+	chmod +x /home/$USER/chech.sh
+	crontab -l > mycron
+	echo "*/30 * * * * /home/$USER/check.sh"
+	crontab mycron
+	rm mycron
+fi
+}
+
+
 amiroot () {
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
@@ -401,11 +665,10 @@ menu () {
 while :
 do
     clear
-    
 echo "==================================================="
 echo "==           SHEKEL Wallet Installer             =="
 echo "==     For Ubuntu 14.04 or 16.04 or CentOS7      =="
-echo "==                version 1.8                    =="
+echo "==                version 2.0                    =="
 echo "==   Donate:JQJ1GanDU3c5RZwNjBXk68wFdxEJKLwWZU   =="
 echo "==                                               =="
 echo "==         Copyright Cryptojatt(c) 2018          ==" 
@@ -419,12 +682,13 @@ echo "JQJ1GanDU3c5RZwNjBXk68wFdxEJKLwWZU"
 echo ""
 sleep 2
 amiroot
-cat<<EOF
+cat <<EOF
     Please enter your choice:
 
     Install Shekel Wallet & Set Up Masternode (1)
     Upgrade Shekel Wallet & Set Up Masternode (2)
-    Install Systemd Script		      (3)
+    Install Systemd Service		      (3)
+    Install Shekel check		      (4)
            				      (Q)uit
     ------------------------------
 EOF
@@ -432,7 +696,8 @@ EOF
     case "$REPLY" in
     "1")  install_masternode ;;
     "2")  upgrade_masternode ;;
-    "3")  echo "To be added" ;;
+    "3")  install_service ;;
+    "4")  install_check ;;
     "Q")  exit                      ;;
     "q")  echo "case sensitive!!"   ;; 
      * )  echo "invalid option"     ;;
